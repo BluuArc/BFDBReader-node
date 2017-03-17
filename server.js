85a44d530e349ca310d26f35bab83775cf2bf4b6
Joshua Castor <jcasto3@uic.edu>
2017-03-17 09:05:36 -0500

    Add merge_databases function

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 var fs = require('fs');
 
-//on-going database that is a combination of 3 other databases (GL,EU,JP)
-var master_list = {
-    unit: null,
-    item: null,
-    load: load_database()
-};
-
 //asynchronous file load, used for updating after database is built
 function asynchr_json_load(file, callbackFn){
     console.log("opening " + __dirname + "/" + file);
@@ -35,19 +28,50 @@ function asynchr_json_write(file, data){
     });
 }
 
+//add in anything in db_sub that is not in db_main
+function merge_databases(db_main, db_sub, server){
+    var local_obj = JSON.parse(JSON.stringify(db_main));
+    for(o in db_sub){ //iterate through everything in object
+        if(local_obj[o] != undefined){ //exists, so just add date add time
+            if(local_obj[o]["server"].indexOf(server) == -1){
+                local_obj[o]["server"].push(server);
+                local_obj[o]["db_add_time"].push(new Date().toUTCString());
+            }
+        }else{ //doesn't exist, so add it and date add time
+            local_obj[o] = db_sub[o];
+            local_obj[o].server = [server];
+            local_obj[o]["db_add_time"] = [new Date().toUTCString()];
+        }
+    }
+    return local_obj;
+}
+
 //load database from a file or files
-function load_database(){
+function load_database(master_obj){
     //open unit
+    console.log("Loading individual unit databases...");
     var global = synchr_json_load('info-gl.json');
     var japan = synchr_json_load('info-jp.json');
     var europe = synchr_json_load('info-eu.json');
-    master_list.unit = global;
-    // merge_databases(master_list.unit, japan);
-    // merge_databases(master_list.unit, europe);
+    console.log("Merging unit databases...")
+    master_obj["unit"] = {};
+    master_obj["unit"] = merge_databases(master_obj.unit, global, 'gl');
+    master_obj["unit"] = merge_databases(master_obj.unit, europe, 'eu');
+    master_obj["unit"] = merge_databases(master_obj.unit, japan, 'jp');
 
     //open item
 }
 
+//on-going database that is a combination of 3 other databases (GL,EU,JP)
+var master_list = {
+    unit: {},
+    item: {},
+};
 
+load_database(master_list);
 
-
+for(u in master_list.unit){
+    if(master_list.unit[u]["server"].indexOf('jp') > -1)
+        for(s in master_list.unit[u]["server"])
+            console.log(master_list.unit[u]["name"] + ": Added to " + master_list.unit[u]["server"][s] + " at " + master_list.unit[u]['db_add_time'][s]);
+}
