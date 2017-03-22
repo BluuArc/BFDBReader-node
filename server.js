4914512e1640fda55ce6e9d65ac7fad8cd6e9fc7
Joshua Castor <jcasto3@uic.edu>
2017-03-21 22:31:12 -0500

    Add master DB file saving and some API functionality

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var fs = require('fs');
 var underscore = require('underscore');
 var compression = require('compression');
+var bodyParser = require('body-parser');
 var express = require('express'),
     app = express();
 
-app.use(compression())
+app.use(compression());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//on-going database that is a combination of 3 other databases (GL,EU,JP)
+var master_list = {
+    unit: {},
+    item: {},
+};
 
 //asynchronous file load, used for updating after database is built
 function asynchr_json_load(file, callbackFn){
@@ -14,7 +22,6 @@ function asynchr_json_load(file, callbackFn){
             console.log(err);
             callbackFn(null);
         }
-        // console.log(data);
         callbackFn(JSON.parse(data));
     });
 }
@@ -30,6 +37,7 @@ function asynchr_json_write(file, data){
         if(err){
             console.log(err);
         }
+        console.log("Saved " + file);
         return;
     });
 }
@@ -54,30 +62,97 @@ function merge_databases(db_main, db_sub, server){
 
 //load database from a file or files
 function load_database(master_obj){
-    //open unit
-    console.log("Loading individual unit databases...");
-    var global = synchr_json_load('info-gl.json');
-    var japan = synchr_json_load('info-jp.json');
-    var europe = synchr_json_load('info-eu.json');
-    console.log("Merging unit databases...")
     master_obj["unit"] = {};
-    master_obj["unit"] = merge_databases(master_obj.unit, global, 'gl');
-    master_obj["unit"] = merge_databases(master_obj.unit, europe, 'eu');
-    master_obj["unit"] = merge_databases(master_obj.unit, japan, 'jp');
+    //open unit
+    try{
+        console.log("Loading master unit databases...");
+        master_obj["unit"] = synchr_json_load('info-master.json');
+    }catch(err){ 
+        console.log("Master database not found. Loading individual unit databases...");
+        var global = synchr_json_load('info-gl.json');
+        var japan = synchr_json_load('info-jp.json');
+        var europe = synchr_json_load('info-eu.json');
+        console.log("Merging unit databases...")
+        master_obj["unit"] = merge_databases(master_obj.unit, global, 'gl');
+        master_obj["unit"] = merge_databases(master_obj.unit, europe, 'eu');
+        master_obj["unit"] = merge_databases(master_obj.unit, japan, 'jp');
+        asynchr_json_write('info-master.json', JSON.stringify(master_obj["unit"]));
+    }
 
     //open item
 }
 
-//on-going database that is a combination of 3 other databases (GL,EU,JP)
-var master_list = {
-    unit: {},
-    item: {},
-};
+app.get('/', function(request, response){
+    response.end("<h1>Hello World</h1>");
+});
 
-load_database(master_list);
+app.get('/unit/:id', function(request, response){
+    var unit = master_list.unit[request.params.id];
+    if(unit == undefined)  
+        response.end(JSON.stringify({error: request.params.id + " is not found"}));
+    else
+        response.end(JSON.stringify(master_list.unit[request.params.id]));
+});
 
-for(u in master_list.unit){
-    if(master_list.unit[u]["server"].indexOf('jp') > -1)
-        for(s in master_list.unit[u]["server"])
-            console.log(master_list.unit[u]["name"] + ": Added to " + master_list.unit[u]["server"][s] + " at " + master_list.unit[u]['db_add_time'][s]);
+app.get('/search', function(request,response){
+    response.sendFile(__dirname + "/" + "search.html");
+});
+
+//get the corresponding unit value of a given query
+function get_query_value(field, query, unit){
+    try{
+        switch(name){
+            case 'unit_name_id': return unit["guide_id"] + ": " + unit["name"].toLowerCase() + " (" + unit["id"]+")";
+            case 'rarity': return unit["rarity"].toString();
+            case 'element': return unit["element"].toLowerCase();
+            case 'gender': return unit["gender"].toLowerCase();
+            case 'move_speed': return unit["movement"]["skill"]["move speed type"].toLowerCase();
+            case 'ls_name': return (unit["leader skill"]["name"] + "* - " + unit["leader skill"]["desc"]).toLowerCase();
+            case 'ls_effect': return JSON.stringify(unit["leader skill"]["effects"]);
+            case 'bb_name': return (unit["bb"]["name"] + "* - " + unit["bb"]["desc"]).toLowerCase();
+            case 'bb_effect': return JSON.stringify(unit["bb"]["effects"]);
+            case 'sbb_name': return (unit["sbb"]["name"] + "* - " + unit["sbb"]["desc"]).toLowerCase();
+            case 'sbb_effect': return JSON.stringify(unit["sbb"]["effects"]);
+            case 'ubb_name': return (unit["ubb"]["name"] + "* - " + unit["ubb"]["desc"]).toLowerCase();
+            case 'ubb_effect': return JSON.stringify(unit["ubb"]["effects"]);
+            case 'es_name': return (unit["extra skill"]["name"] + "* - " + unit["extra skill"]["desc"]).toLowerCase();
+            case 'es_effect': return JSON.stringify(unit["extra skill"]["effects"]);
+            case 'sp_name':
+                var result = "";
+                for(sp in unit["skills"]){
+                    result += unit["skills"][sp]["skill"]["desc"] + "\n";
+                }
+                return result;
+            case 'sp_effect':
+                var result = "";
+                for (sp in unit["skills"]) {
+                    result += JSON.stringify(unit["skills"][sp]["skill"]["effects"]) + "\n";
+                }
+                return result;
+            default: return "";
+        }
+    }catch(err){
+        console.log(err);
+        return "";
+    }
 }
+
+app.get('/search/options', function(request,response){
+    console.log(request.query);
+    var query = request.query;
+    for(q in query){
+        var curQuery = query[q].toLowerCase();
+        // if  (curQuery == '' || (q == 'element' && curQuery == 'any') ||
+        //     (q == 'gender' && curQuery == 'any')
+    }
+    response.end("Received request");
+})
+
+var server = app.listen(8081, '127.0.0.1', function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    load_database(master_list);
+
+    console.log("Server listening at http://%s:%s", host, port);
+});
